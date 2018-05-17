dab04c2e031a9fa07bc710216d4c7182c1bdd24e
Priit Kallas <kallaspriit@gmail.com>
2018-05-17 15:20:58 +0300

    Improved union types.

--- src/16-typeof.ts
+++ src/16-typeof.ts
@@ -1,26 +1,29 @@
-interface IConfig {
-  [x: string]: number | string | IConfig;
-}
+namespace Example16 {
+  interface IConfig {
+    [x: string]: number | string | IConfig; // recursive
+  }
 
-function logConfig(config: IConfig) {
-  Object.keys(config).forEach(key => {
-    const value = config[key];
+  function logConfig(config: IConfig) {
+    Object.keys(config).forEach(key => {
+      const value = config[key];
 
-    if (typeof value === "number") {
-      console.log(`config number ${key} = ${value}`);
-    } else if (typeof value === "string") {
-      console.log(`config string ${key} = ${value}`);
-    } else {
-      console.log(`config object ${key} = ${JSON.stringify(value)}`);
+      if (typeof value === "number") {
+        console.log(`config number ${key} = ${value}`);
+      } else if (typeof value === "string") {
+        console.log(`config string ${key} = ${value}`);
+      } else {
+        console.log(`config object ${key} = ${JSON.stringify(value)}`);
+      }
+    });
+  }
+
+  logConfig({
+    host: "localhost",
+    port: 3000,
+    auth: {
+      username: "foo",
+      password: "bar"
+      // startDate: new Date() // Date type not allowed
     }
   });
 }
-
-logConfig({
-  host: "localhost",
-  port: 3000,
-  auth: {
-    username: "foo",
-    password: "bar"
-  }
-});
--- src/17-intersection-and-union-types.ts
+++ src/17-intersection-and-union-types.ts
@@ -1,83 +1,117 @@
-type Email = string;
-type Password = string;
-type Age = number;
+namespace Example17 {
+  // you can alias built-in types
+  type Name = string;
 
-interface IUserAccount {
-  email: Email;
-  password: Password;
-  age: Age;
-}
-
-interface IHasName {
-  name: string;
-}
-
-interface ICanWalk {
-  walk(): void;
-}
-
-interface ICanSwim {
-  swim(): void;
-}
-
-// intersection (not union!) type
-type IFerret = IHasName & ICanWalk & ICanSwim;
-
-class SableFerret implements IFerret {
-  public constructor(public name: string) {}
-
-  public walk() {
-    console.log(`sable ferret "${this.name} is walking"`);
+  interface IHasName {
+    name: Name;
   }
 
-  public swim() {
-    console.log(`sable ferret "${this.name} is swimming"`);
-  }
-}
-
-const spark = new SableFerret("Spark");
-
-spark.walk();
-spark.swim();
-
-function takeForAWalk(animal: ICanWalk) {
-  animal.walk();
-}
-
-takeForAWalk(spark);
-
-// union type (either a Date OR a number)
-function getSecondsSince(time: Date | number) {
-  if (time instanceof Date) {
-    // instanceof type guard makes sure that TypeScript knows that time is now a Date
-    return Date.now() - time.getTime();
+  interface ICanWalk {
+    speed: number;
+    walk(): void;
   }
 
-  return Date.now() - time;
-}
-
-// these give the same result
-console.log(
-  "getSecondsSince(new Date(Date.now() - 10))",
-  getSecondsSince(new Date(Date.now() - 10))
-);
-console.log(
-  "getSecondsSince(Date.now() - 10))",
-  getSecondsSince(Date.now() - 10)
-);
-
-// another union example
-type Name = string;
-type NameResolver = () => string;
-type NameOrResolver = Name | NameResolver;
-
-function getName(name: NameOrResolver): Name {
-  if (typeof name === "string") {
-    return name;
-  } else {
-    return name();
+  interface ICanSwim {
+    speed: number;
+    // speed: string; // declaration merging - speed: string & number
+    swim(): void;
   }
-}
 
-console.log("name1", getName("test 1"));
-console.log("name2", getName(() => "test 2"));
+  // intersection (not union!) type ()
+  type IFerret = IHasName & ICanWalk & ICanSwim;
+
+  class Ferret implements IFerret {
+    public constructor(public name: string, public speed: number) {}
+
+    public walk() {
+      console.log(`sable ferret "${this.name} is walking"`);
+    }
+
+    public swim() {
+      console.log(`sable ferret "${this.name} is swimming"`);
+    }
+  }
+
+  const spark = new Ferret("Spark", 10);
+
+  spark.walk();
+  spark.swim();
+
+  function takeForAWalk(animal: ICanWalk) {
+    animal.walk();
+  }
+
+  takeForAWalk(spark);
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // union type (either a Date OR a number)
+  function getSecondsSince(time: Date | number) {
+    if (time instanceof Date) {
+      // instanceof type guard makes sure that TypeScript knows that time is now a Date
+      return Date.now() - time.getTime();
+    }
+
+    return Date.now() - time;
+  }
+
+  // these give the same result
+  console.log(
+    "getSecondsSince(new Date(Date.now() - 10))",
+    getSecondsSince(new Date(Date.now() - 10))
+  );
+  console.log(
+    "getSecondsSince(Date.now() - 10))",
+    getSecondsSince(Date.now() - 10)
+  );
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // another union example using generics
+  type ValueResolver<T> = T | (() => T) | (() => Promise<T>);
+
+  interface IConfig {
+    port: ValueResolver<number>;
+  }
+
+  const config1: IConfig = {
+    port: 8000
+  };
+
+  const config2: IConfig = {
+    port: () => 8001
+  };
+
+  const config3: IConfig = {
+    port: async () =>
+      new Promise<number>(resolve => setTimeout(() => resolve(8002), 1000))
+  };
+
+  async function resolveValue<T>(value: ValueResolver<T>): Promise<T> {
+    if (value instanceof Promise) {
+      return value;
+    } else if (typeof value === "function") {
+      return value();
+    }
+
+    return value;
+  }
+
+  async function setup(config: IConfig) {
+    const port = await resolveValue(config.port);
+
+    console.log(`setup on port ${port}`);
+  }
+
+  (async () => {
+    console.log({
+      config1,
+      config2,
+      config3
+    });
+
+    await setup(config1);
+    await setup(config2);
+    await setup(config3);
+  })().catch(console.error);
+}
